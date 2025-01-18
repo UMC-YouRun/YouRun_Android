07c463bedb88443d6f6b49f3d1f74e915de5c621
Je-0204 <dlej0204@naver.com>
2025-01-18 20:32:59 +0900

    feat/앱 설명 화면 UI 추가(제일 처음 화면) 및 compose 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,17 +40,11 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation("androidx.core:core:1.12.0")
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/example/yourun/view/AppExpActivity.kt
+++ app/src/main/java/com/example/yourun/view/AppExpActivity.kt
@@ -2,9 +2,13 @@ package com.example.yourun.view
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.yourun.R
 
-class MainActivity : Activity() {
+class AppExpActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_exp)
+
     }
 }
