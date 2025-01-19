7faede448d3f5cdb6aa1e9501ad4e538cbdfa632
Je-0204 <dlej0204@naver.com>
2025-01-19 17:58:08 +0900

    feat/앱 설명3 화면 UI 구현

--- app/src/main/java/com/example/yourun/view/AppExpFragment3.kt
+++ app/src/main/java/com/example/yourun/view/AppExpFragment3.kt
@@ -1,10 +1,17 @@
 package com.example.yourun.view
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.addCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.yourun.R
 
@@ -20,18 +27,44 @@ class AppExpFragment3 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /* view.findViewById<Button>(R.id.next_btn).setOnClickListener {
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener {
             val nextFragment = AppExpFragment4()
             parentFragmentManager.beginTransaction()
                 .replace(R.id.app_exp_fragment_container, nextFragment)
                 .addToBackStack(null)
-                .setReorderingAllowed(true)
                 .commit()
-        } */
+        }
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+        view.findViewById<ImageButton>(R.id.back_button).setOnClickListener {
             parentFragmentManager.popBackStack()
         }
+
+        // android back button
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (parentFragmentManager.backStackEntryCount > 0) {
+                parentFragmentManager.popBackStack()
+            } else {
+                requireActivity().finish()
+            }
+        }
+
+        val txt_mate_win = view.findViewById<TextView>(R.id.txt_app_exp_mate_win)
+
+        val fullText = txt_mate_win.text.toString()
+        val targetText = "러닝 메이트,"
+        val spannableString = SpannableString(fullText)
+        val startIndex = fullText.indexOf(targetText)
+        val endIndex = startIndex + targetText.length
+
+        if (startIndex >= 0) {
+            spannableString.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.text_purple)),
+                startIndex,
+                endIndex,
+                SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+        txt_mate_win.text = spannableString
 
     }
 }
