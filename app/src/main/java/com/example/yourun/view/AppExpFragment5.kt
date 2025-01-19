beefdf0dbd4e62671ae6b89c82affce49568266b
Je-0204 <dlej0204@naver.com>
2025-01-19 22:57:43 +0900

    feat/앱 설명5 화면 UI 구현 및 앱 아이콘 교체

--- app/src/main/java/com/example/yourun/view/AppExpFragment5.kt
+++ app/src/main/java/com/example/yourun/view/AppExpFragment5.kt
@@ -1,7 +1,66 @@
 package com.example.yourun.view
 
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.addCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.yourun.R
 
 class AppExpFragment5 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_app_exp5, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener {
+            // 성향 테스트 activity로 이동
+        }
+
+        view.findViewById<ImageButton>(R.id.back_button).setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        // android back button
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (parentFragmentManager.backStackEntryCount > 0) {
+                parentFragmentManager.popBackStack()
+            } else {
+                requireActivity().finish()
+            }
+        }
+
+        val txt_how_run = view.findViewById<TextView>(R.id.txt_app_exp_how_run)
+
+        val fullText = txt_how_run.text.toString()
+        val targetText = "얼마나 러닝"
+        val spannableString = SpannableString(fullText)
+        val startIndex = fullText.indexOf(targetText)
+        val endIndex = startIndex + targetText.length
+
+        if (startIndex >= 0) {
+            spannableString.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.text_purple)),
+                startIndex,
+                endIndex,
+                SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+        txt_how_run.text = spannableString
+    }
 
 }
