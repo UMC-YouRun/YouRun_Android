513cf956839767f3c08ffd778f331418cb8bc179
muunseo <mkogo90@naver.com>
2025-01-22 13:37:41 +0900

    feature/#9 마이런 페이지 UI 구현

---
+++ app/src/main/java/com/example/yourun/view/activities/MyRun1Activity.kt
@@ -0,0 +1,17 @@
+package com.example.yourun.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yourun.databinding.ActivityMyrun1Binding
+
+class MyRun1Activity : AppCompatActivity () {
+    private lateinit var binding: ActivityMyrun1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyrun1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
