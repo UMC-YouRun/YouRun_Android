f3e785e4007e410605f6d11067c62d2d09c72372
Ari1022 <imemine6934@gmail.com>
2025-01-24 12:37:12 +0900

    feat : 챌린지 상세화면 디자인(크루) 구현
    - 앞에 연결될 목록 화면은 구현 미완료
    - 바텀 내비게이션 추가 예정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
---
+++ app/src/main/java/com/example/yourun/view/activities/ChallengeListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yourun.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yourun.R
+
+class ChallengeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_list)
+    }
+}
