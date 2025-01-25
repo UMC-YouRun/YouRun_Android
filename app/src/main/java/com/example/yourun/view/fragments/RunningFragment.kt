639b9aa480cb3ed7f5a4074ef2a7dd2223088eb8
Je-0204 <dlej0204@naver.com>
2025-01-25 15:58:11 +0900

    feat/러닝 진행 초기 화면 UI 구현

--- app/src/main/java/com/example/yourun/view/fragments/RunningFragment.kt
+++ app/src/main/java/com/example/yourun/view/fragments/RunningFragment.kt
@@ -1,6 +1,37 @@
 package com.example.yourun.view.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.yourun.R
 
 class RunningFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_running, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val topBarView = view.findViewById<View>(R.id.running_top_bar)
+        val titleTextView = topBarView.findViewById<TextView>(R.id.txt_top_bar)
+        titleTextView.text = "러닝 시작하기"
+
+        view.findViewById<ImageButton>(R.id.btn_mate_select).setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.running_fragment_container, RunningMateSelectFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/yourun/view/fragments/RunningMateSelectFragment.kt
@@ -0,0 +1,28 @@
+package com.example.yourun.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import com.example.yourun.R
+
+class RunningMateSelectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_running_mate_select, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageButton>(R.id.back_button).setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+    }
+}
