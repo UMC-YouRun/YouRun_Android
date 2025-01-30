40504b9b5b61c948e0bb719e8f1ca0e46849d432
Ari1022 <imemine6934@gmail.com>
2025-01-30 23:56:02 +0900

    feature : 메이트 화면 구현
    - 레이아웃 파일 저장용 커밋
    - MateActivity와 MateAdapter 수정 필요

---
+++ app/src/main/java/com/example/yourun/view/activities/MateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yourun.view.activities
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.yourun.R
+
+class MateActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mate)
+    }
+}
