2ff4b4aa32102173ebb38f49a46680f8e0b0743f
Je-0204 <dlej0204@naver.com>
2025-01-31 12:17:32 +0900

    feat/카카오 SDK 의존성 추가, 인터넷 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,10 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.airbnb.android:lottie:6.0.0")
+    implementation ("com.kakao.sdk:v2-all:2.20.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.airbnb.android:lottie:6.0.0")
+
 }
