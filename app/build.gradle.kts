2050f093297dc1a6130fb8b8879be03c579ab6eb
Ari1022 <imemine6934@gmail.com>
2025-02-01 21:29:43 +0900

    chore : 초기 버전저장

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.airbnb.android:lottie:6.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
 }
