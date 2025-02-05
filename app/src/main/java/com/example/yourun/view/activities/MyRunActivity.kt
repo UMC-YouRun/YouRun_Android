003beb81810758406859ceba5e4b858e02c1192e
muunseo <mkogo90@naver.com>
2025-02-05 11:53:19 +0900

    myrun페이지 fragment로 변경 및 home화면과 연결

---
+++ app/src/main/java/com/example/yourun/view/activities/MyRunActivity.kt
@@ -0,0 +1,28 @@
+package com.example.yourun.view.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yourun.R
+import com.example.yourun.databinding.ActivityMyrunBinding
+import com.example.yourun.view.fragments.MyRunFragment
+
+class MyRunActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMyrunBinding  // 바인딩 객체 선언
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // View Binding 초기화
+        binding = ActivityMyrunBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Fragment 추가
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MyRunFragment())
+            .commit()
+
+
+    }
+}
